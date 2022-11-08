2d87e8339ebd38e1c9c4715bd47dbfee48461c0c
Nuttayos Suvisuttikasame <nuttayossuvisuttikasame@MacBook-Pro-khxngNuttayos.local>
2022-11-08 22:27:46 +0700

    section2 input

--- main.go
+++ main.go
@@ -1,9 +1,13 @@
 package main
 
 //library we'll use need to import here
-import "fmt"
+import (
+	"fmt"
+	"practice1/myTest"
+)
 
 //go always run in main function
 func main() {
 	fmt.Println("hello world")
+	test2.Test2()
 }
---
+++ myTest/test2.go
@@ -0,0 +1,20 @@
+package test2
+
+import (
+	"fmt"
+	"bufio"
+	"os"
+	"log"
+)
+
+//in order to export need to capitalize first char
+func Test2() {
+	fmt.Println("What is your name?")
+	reader := bufio.NewReader(os.Stdin)
+	name, err := reader.ReadString('\n')
+	if err == nil {
+		fmt.Printf("Hello %v \n",name)
+	} else {
+		log.Fatal(err)
+	}
+}
